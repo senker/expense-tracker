8da87e432532387bb36207fbebce1b6c40983f1e
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2024-12-05 16:56:19 +0100

    Update ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -86,7 +86,7 @@ const ExpenseForm = (props) => {
           <input
             type="date"
             min="2022-01-01"
-            max="2023-12-31"
+            max="2024-12-31"
             value={enteredDate}
             onChange={dateChangeHandler}
           />
